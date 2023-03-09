76b304f2be55a5003646cc6e8b3af5bece745712
yoshiki_murakami <eiim.yoshiki@gmail.com>
2023-03-09 22:32:27 +0900

    change line text

--- hello_world/chat_gpt.rb
+++ hello_world/chat_gpt.rb
@@ -4,13 +4,15 @@ require "openai"
 class ChatGpt
   class << self
     def get_text(event)
-      message = event[:body]
+      event_body_json = JSON.parse(event["body"])
+      req_text = event_body_json["events"][0]["message"]["text"]
+
       client = OpenAI::Client.new(access_token: ENV['CHAT_GPT_API_KEY'])
 
       response = client.chat(
         parameters: {
           model: "gpt-3.5-turbo",
-          messages: [{ role: "user", content: "こんにちは"}],
+          messages: [{ role: "user", content: req_text}],
           temperature: 0.7,
         })
 
